4de5d7ee88746bfc01cc928cbcd3ef220e66174f
Nylon <diego.gomes87@gmail.com>
2015-11-05 12:08:38 -0200

    Initial Commit

---
+++ UsandoJsonData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UsandoJsonData
+//
+//  Created by Diego Gomes on 05/11/2015.
+//  Copyright (c) 2015 Nylon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
