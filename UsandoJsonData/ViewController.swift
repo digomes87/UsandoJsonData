6f5e74d453b3b0e6e0d2370f8802b5a44e6a346a
Nylon <diego.gomes87@gmail.com>
2015-11-05 12:21:11 -0200

    Jason
    Fazendo uso do Json Data.
    Muitos Apps usando a geolocalização pra alguma funcionalidade.
    Exemplo serviço como o Tinder que usa um raio pra procurar pessoas
    dentro do seu perfil de interesse.
    Ou serviço de busca de comida,buscas os estabelecimentos no raio de
    acordo com o que vc quer.

--- UsandoJsonData/ViewController.swift
+++ UsandoJsonData/ViewController.swift
@@ -13,6 +13,22 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://www.telize.com/geoip"
+        let url = NSURL(string: urlPath)
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+            
+            if   error != nil {
+                println(error)
+            }else{
+                println(data)
+            }
+        })
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
